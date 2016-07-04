2c48566b64300d0046aef909fc6bb37ea51444d6
Alexei Bendebury <alyosha.be@gmail.com>
2016-07-04 11:27:35 -0700

    Fixed typo in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,11 @@ if [ $TRAVIS_BRANCH == 'develop' ] ; then
 
     mv deploy_key ~/.ssh/id_rsa
 
-    git remote add deploy "deploy@104.236.112.22:/home/www/quizApp"
+    git remote add deploy "deploy@104.236.112.220:/home/www/quizApp"
     git config user.name "Travis CI"
     git config user.email "alexeibendebury+travis@gmail.com"
 
-    # This may seem bad, but since this is a public repo I am not concerned
-    # about MITM attacks.
-    yes | git push --force deploy develop 
+    git push --force deploy develop 
 else
     echo "Not deploying"
 fi
