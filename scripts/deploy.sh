72a342355c3f083886bbc43d5f82182411bcb006
Alexei Bendebury <alyosha.be@gmail.com>
2016-07-04 11:00:31 -0700

    Setting up deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+if [ %TRAVIS_BRANCH == 'develop' ] ; then
+    chmod 600 deploy_key
+
+    mv deploy_key ~/.ssh/id_rsa
+
+    git remote add deploy "deploy@104.236.112.22:/home/www/quizApp"
+    git config user.name "Travis CI"
+    git config user.email "alexeibendebury+travis@gmail.com"
+
+    git push --force deploy develop
+else
+    echo "Not deploying"
+fi
