f5f85582ecfae6cabaefcf4c6e782bb5098fa8a4
PlasmaSheep <plasmasheep@gmail.com>
2016-07-07 13:21:28 -0700

    moved DateTimeWidget rendering into a macro

--- quizApp/forms/experiments.py
+++ quizApp/forms/experiments.py
@@ -4,29 +4,6 @@ from wtforms import StringField, DateTimeField, SubmitField, HiddenField, \
 from wtforms.validators import DataRequired
 from wtforms.widgets.core import HTMLString, Input
 
-class DateTimeWidget(object):
-    def __call__(self, field, **kwargs):
-        output = ('<div class="input-group date" id="{}">'
-                  '<input type="text" class="form-control {}"'
-                  '    name="{}", value="{}"/>'
-                  '<span class="input-group-addon">'
-                  '    <span class="glyphicon'
-                  '    glyphicon-calendar"></span>'
-                  '    </span>'
-                  '</div>'
-                  '<script type="text/javascript">'
-                  '    $(function () {{'
-                  '        $("#{}").datetimepicker({{'
-                  '             format: "YYYY-MM-DD HH:mm:ss"'
-                  '         }});'
-                  '    }});'
-                  '</script>').format(field.id,
-                                      kwargs.pop("class_", ""),
-                                      field.name,
-                                      kwargs.pop("value", ""),
-                                      field.id)
-        return HTMLString(output)
-
 class TextAreaWidget(Input):
     input_type = "text"
     def __call__(self, field, **kwargs):
@@ -53,10 +30,8 @@ class LikertWidget(object):
 
 class CreateExperimentForm(Form):
     name = StringField("Name", validators=[DataRequired()])
-    start = DateTimeField("Start time", widget=DateTimeWidget(),
-                          validators=[DataRequired()])
-    stop = DateTimeField("Stop time", widget=DateTimeWidget(),
-                        validators=[DataRequired()])
+    start = DateTimeField("Start time", validators=[DataRequired()])
+    stop = DateTimeField("Stop time", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 class DeleteExperimentForm(Form):
