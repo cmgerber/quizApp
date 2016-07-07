05b2732083840cfdc9314bada796ee8f93b3378f
PlasmaSheep <plasmasheep@gmail.com>
2016-07-07 15:30:58 -0700

    Added coverage checking

--- runtests.py
+++ runtests.py
@@ -6,7 +6,9 @@ import sys
 
 def runtests():
     os.environ["APP_CONFIG"] = "testing"
-    return pytest.main(["--cov=quizApp", "tests/"] + sys.argv)
+    return pytest.main(["--cov-report", "xml:.coverage",
+                        "--cov=quizApp",
+                        "tests/"] + sys.argv)
 
 if __name__ == "__main__":
     exit(runtests())
