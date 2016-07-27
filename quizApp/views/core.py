22f9d72cb90d8a9d2aa066560d943ad8787f297d
PlasmaSheep <plasmasheep@gmail.com>
2016-07-27 14:11:18 -0700

    Factored out import/export code

--- quizApp/views/core.py
+++ quizApp/views/core.py
@@ -5,12 +5,11 @@ import os
 
 from flask import Blueprint, render_template, send_file
 from openpyxl import Workbook
-from sqlalchemy import inspect
-from sqlalchemy.orm.properties import ColumnProperty, RelationshipProperty
 from flask_security import roles_required
 
 from quizApp import models
 from quizApp.config import basedir
+from quizApp.views import import_export
 
 
 core = Blueprint("core", __name__, url_prefix="/")
@@ -44,44 +43,12 @@ def export():
     for sheet_name, query in sheets.iteritems():
         current_sheet = workbook.create_sheet()
         current_sheet.title = sheet_name
-        sheet_data = object_list_to_sheet(query.all())
-        write_list_to_sheet(sheet_data, current_sheet)
+        sheet_data = import_export.object_list_to_sheet(query.all())
+        import_export.write_list_to_sheet(sheet_data, current_sheet)
 
     file_name = os.path.join(basedir, "export.xlsx")
     workbook.save(file_name)
     return send_file(file_name, as_attachment=True)
-
-
-def object_list_to_sheet(object_list):
-    """Given a list of objects, iterate over all of them and create an xlsx
-    sheet.
-
-    The first row returned will be a header row. All fields will be included
-    unless a field contains export-include: False in its info attibute.
-    """
-    headers = []
-    rows = []
-    for obj in object_list:
-        row = [""]*len(headers)
-        for column, prop in inspect(type(obj)).attrs.items():
-            include = prop.info.get("export_include", True)
-
-            if not isinstance(prop, ColumnProperty):
-                continue
-
-            if not include:
-                continue
-
-            if column not in headers:
-                headers.append(column)
-                row.append("")
-            index = headers.index(column)
-
-            row[index] = getattr(obj, column)
-        rows.append(row)
-    sheet = [headers]
-    sheet.extend(rows)
-    return sheet
 
 
 @core.route('import_template')
@@ -101,7 +68,7 @@ def import_template():
         ["Simply add in your data in the rows undeneath it."],
         ["Use IDs from the export sheet to populate relationship columns."],
         [("If you want multiple objects in a relation, separate the IDs using"
-          "commas.")],
+          " commas.")],
     ]
 
     workbook = Workbook()
@@ -110,44 +77,13 @@ def import_template():
     for sheet_name, model in sheets.iteritems():
         current_sheet = workbook.create_sheet()
         current_sheet.title = sheet_name
-        headers = model_to_sheet_headers(model)
-        write_list_to_sheet(headers, current_sheet)
+        headers = import_export.model_to_sheet_headers(model)
+        import_export.write_list_to_sheet(headers, current_sheet)
 
     current_sheet = workbook.create_sheet()
     current_sheet.title = "Documentation"
-    write_list_to_sheet(documentation, current_sheet)
+    import_export.write_list_to_sheet(documentation, current_sheet)
 
     file_name = os.path.join(basedir, "import-template.xlsx")
     workbook.save(file_name)
     return send_file(file_name, as_attachment=True)
-
-
-def model_to_sheet_headers(model):
-    """Given a model, put all columns whose info contains "export-include":
-    True into a list of headers.
-    """
-
-    headers = []
-    for column, prop in inspect(model).attrs.items():
-        if isinstance(prop, ColumnProperty):
-            info = prop.columns[0].info
-        elif isinstance(prop, RelationshipProperty):
-            info = prop.info
-
-        include = info.get("import_include", False)
-
-        if include:
-            headers.append(column)
-
-    return [headers]
-
-
-def write_list_to_sheet(data_list, sheet):
-    """Given a list and a sheet, write out the list to the sheet.
-    The list should be two dimensional, with each list in the list representing
-    a row.
-    """
-
-    for r in xrange(1, len(data_list) + 1):
-        for c in xrange(1, len(data_list[r - 1]) + 1):
-            sheet.cell(row=r, column=c).value = data_list[r - 1][c - 1]
---
+++ quizApp/views/import_export.py
@@ -0,0 +1,178 @@
+"""Functions for importing and exporting data via XLSX files.
+"""
+
+from collections import OrderedDict, defaultdict
+from sqlalchemy import inspect
+from sqlalchemy.orm.interfaces import ONETOMANY, MANYTOMANY
+from sqlalchemy.orm.properties import ColumnProperty, RelationshipProperty
+
+from quizApp import db
+from quizApp import models
+
+
+def model_to_sheet_headers(model):
+    """Given a model, put all columns whose info contains "export-include":
+    True into a list of headers.
+    """
+
+    headers = []
+    for column, prop in inspect(model).attrs.items():
+        if isinstance(prop, ColumnProperty):
+            info = prop.columns[0].info
+        elif isinstance(prop, RelationshipProperty):
+            info = prop.info
+
+        include = info.get("import_include", False)
+
+        if include:
+            headers.append(column)
+
+    return [headers]
+
+
+def write_list_to_sheet(data_list, sheet):
+    """Given a list and a sheet, write out the list to the sheet.
+    The list should be two dimensional, with each list in the list representing
+    a row.
+    """
+
+    for r in xrange(1, len(data_list) + 1):
+        for c in xrange(1, len(data_list[r - 1]) + 1):
+            sheet.cell(row=r, column=c).value = data_list[r - 1][c - 1]
+
+
+def object_list_to_sheet(object_list):
+    """Given a list of objects, iterate over all of them and create an xlsx
+    sheet.
+
+    The first row returned will be a header row. All fields will be included
+    unless a field contains export-include: False in its info attibute.
+
+    Note: does not currently support printing relationships.
+    """
+    headers = []
+    rows = []
+    for obj in object_list:
+        row = [""] * len(headers)
+        for column, prop in inspect(type(obj)).attrs.items():
+            include = prop.info.get("export_include", True)
+
+            if not isinstance(prop, ColumnProperty):
+                continue
+
+            if not include:
+                continue
+
+            if column not in headers:
+                headers.append(column)
+                row.append("")
+            index = headers.index(column)
+
+            row[index] = getattr(obj, column)
+        rows.append(row)
+    sheet = [headers]
+    sheet.extend(rows)
+    return sheet
+
+
+def populate_field(model, obj, field_name, value, pk_mapping):
+    """Populate a field on a certain object based on the value from an imported
+    spreadsheet.
+
+    This may involve doing a database lookup if the field in question is a
+    relationship field.
+
+    A note on pk_mapping:
+
+    To avoid conflicts between imported PK and existing PK, we do not
+    assign PK's based on user input. However we have to store them
+    because other rows in the user input may be referencing a certain PK. So
+    we store them in a kind of bastard mini-database in memory while we
+    are importing data.
+
+    Arguments:
+        model - The sqlalchemy model that obj is an instance of.
+        obj - The object whose fields need populating.
+        field_name - A string containing the name of the field that should be
+        populated.
+        value - The value of the field, as read from the spreadsheet.
+        pk_mapping - A mapping of any objects created in this import session
+        that value may refer to.
+    """
+    field_attrs = inspect(model).attrs[field_name]
+    field = getattr(model, field_name)
+    column = getattr(obj, field_name)
+    if isinstance(field_attrs, RelationshipProperty):
+        # This is a relationship
+        remote_model = field.property.mapper.class_
+        direction = field.property.direction
+        if direction in (MANYTOMANY, ONETOMANY):
+            values = str(value).split(",")
+            for fk_id in values:
+                fk_id = int(float(fk_id))  # goddamn stupid excel
+                column.append(get_object_from_id(remote_model, fk_id,
+                                                 pk_mapping))
+        else:
+            value = int(float(value))  # goddamn stupid excel
+            setattr(obj, field_name, get_object_from_id(remote_model, value,
+                                                        pk_mapping))
+    elif field.primary_key:
+        pk_mapping[model.__tablename__][int(float(value))] = obj
+    elif isinstance(field_attrs, ColumnProperty):
+        setattr(obj, field_name, value)
+
+
+def get_object_from_id(model, obj_id, pk_mapping):
+    """If the object of type model and id obj_id is in pk_mapping,
+    return it. Otherwise, query the database.
+    """
+    try:
+        return pk_mapping[model.__tablename__][obj_id]
+    except KeyError:
+        return model.query.get(obj_id)
+
+
+def create_assignments_from_workbook(workbook, experiment):
+    """Given an excel workbook, read in the sheets and save them to the
+    database.
+    """
+    models_mapping = OrderedDict([
+        ("Participant Experiments", models.ParticipantExperiment),
+        ("Assignments", models.Assignment),
+    ])
+    pk_mapping = defaultdict(dict)
+
+    for sheet_name, model in models_mapping.iteritems():
+        sheet = workbook.get_sheet_by_name(sheet_name)
+
+        headers = []
+
+        for row_index, row in enumerate(sheet.rows):
+            if row_index == 0:
+                for col_index, cell in enumerate(row):
+                    headers.append(cell.value)
+                continue
+
+            obj = model()
+
+            associate_obj_with_experiment(obj, experiment)
+
+            for col_index, cell in enumerate(row):
+                value = cell.value
+                populate_field(model, obj, headers[col_index], value,
+                               pk_mapping)
+
+            db.session.add(obj)
+
+
+def associate_obj_with_experiment(obj, experiment):
+    """Given an object and an experiment, associate them.
+
+    This is mostly simple but requires some corner cases. For example, since we
+    do not generate participant experiments with user IDs but rather as
+    templates, we store them in Experiment.participant_experiment_templates.
+    """
+    if hasattr(obj, "experiments"):
+        obj.experiments.append(experiment)
+    elif hasattr(obj, "experiment"):
+        obj.experiment = experiment
