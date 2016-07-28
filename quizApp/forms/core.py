42a4ce10754d816b5a34f53df538d4c00ff543a1
Alexei Bendebury <alyosha.be@gmail.com>
2016-07-28 16:01:56 -0700

    Added core forms

---
+++ quizApp/forms/core.py
@@ -0,0 +1,14 @@
+"""Forms for core views.
+"""
+from flask_wtf import Form
+from wtforms import SubmitField, FileField
+from wtforms.validators import DataRequired
+
+class ImportDataForm(Form):
+    """Provide a FileField for an xlsx upload.
+    """
+
+    data = FileField("Import data", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
