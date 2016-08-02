663e1b8d10a9f933f0289180491c3fd74aabc189
PlasmaSheep <plasmasheep@gmail.com>
2016-08-02 13:24:28 -0700

    More flexible post_hits

--- quizApp/views/mturk.py
+++ quizApp/views/mturk.py
@@ -9,5 +9,9 @@ mturk = Blueprint("mturk", __name__, url_prefix="/mturk")
 @mturk.route("/register")
 def register():
     """Register this amturk user.
+
+    From the URL arguments, grab the user ID from amazon and create a user
+    record in our database. Log in the user. Redirect them to the experiment
+    landing page.
     """
     return render_template("mturk/register.html", request=request)
