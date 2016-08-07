3147ad90dc68548fbbf90f1daee29a9271954b99
Alexei Bendebury <alyosha.be@gmail.com>
2016-08-07 11:41:12 -0700

    documentation for manage script

--- manage.py
+++ manage.py
@@ -1,29 +1,30 @@
-import os
-import sys
+#!/usr/bin/env python
+"""Management script for common operations.
+"""
+
 import click
 import pytest
-from sqlalchemy.engine.url import make_url
-from flask import current_app
 from flask.cli import FlaskGroup
-from sqlalchemy.exc import OperationalError
-import pdb
 from quizApp import create_app
 
 
 @click.pass_context
-def get_app(ctx, info):
+def get_app(ctx, _):
+    """Create an app with the correct config.
+    """
     return create_app(ctx.parent.params["config"])
 
 
 @click.option("-c", "--config", default="development")
 @click.group(cls=FlaskGroup, create_app=get_app)
-def cli(config):
+def cli(**_):
+    """Define the top level group.
+    """
     pass
 
 
 @cli.command("test")
-@click.pass_context
-def test(ctx):
+def test():
     """Set the app config to testing and run pytest, passing along command
     line args.
     """
@@ -31,7 +32,6 @@ def test(ctx):
                         "--flake8",
                         "--pylint",
                         "./"])
-
 
 if __name__ == '__main__':
     cli()
