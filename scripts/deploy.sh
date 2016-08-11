77d554b9678ad25a75d323102ca1c441fc2bf1d1
Alexei Bendebury <alyosha.be@gmail.com>
2016-08-11 15:43:42 -0700

    Document deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# This script is used for deploying to our Digital Ocean droplet
+# from Travis CI.
+
 set -x
 
 if [ $TRAVIS_BRANCH == 'develop' ] && [ $TRAVIS_PULL_REQUEST == 'false' ]; then
+    # Prepare the deployment key
     chmod 600 deploy_key
-
     mv deploy_key ~/.ssh/id_rsa
 
+    # Add the server and push
     git remote add deploy "www@104.236.112.220:/home/www/quizApp.git"
     git config user.name "Travis CI"
     git config user.email "alexeibendebury+travis@gmail.com"
