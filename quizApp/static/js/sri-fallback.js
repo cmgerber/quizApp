bbe94ff7a4114ef2f54da445e7499ec5f562cb9d
PlasmaSheep <plasmasheep@gmail.com>
2016-08-10 20:09:01 -0700

    Implemented fallbacks in case cdn is down/compromised

--- quizApp/static/js/sri-fallback.js
+++ quizApp/static/js/sri-fallback.js
@@ -1,3 +1,11 @@
 function resource_error(element) {
-  fallback_url = element.dataset.fallback
+    var fallback_url = element.dataset.fallback;
+    var is_js = fallback_url.substr(fallback_url.length - 3) == ".js";
+
+    if (is_js) {
+        document.write("<script src='" + fallback_url + "'></script>");
+    } else {
+        // Assume we have jquery loaded by now
+        $('head').append("<link href='" + fallback_url + "' rel='stylesheet'>");
+    }
 }
